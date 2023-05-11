6a71ad13d0746d300052fa6bcc5a2ac8b122a66e
Giorgi Tamarashvili <giorgi.tamarashvili.1@btu.edu.ge>
2023-05-11 21:51:03 +0400

    initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import argparse
+import boto3
+from botocore.exceptions import ClientError
+
+
+def create_vpc(client, name):
+    try:
+        vpc = client.create_vpc(CidrBlock='10.0.0.0/16')
+        vpc.create_tags(Tags=[{'Key': 'Name', 'Value': name}])
+        return vpc
+    except ClientError as e:
+        print(e.response)
+        return None
+
+
+def create_igw(client, name):
+    try:
+        igw = client.create_internet_gateway()
+        igw.create_tags(Tags=[{'Key': 'Name', 'Value': f'{name}-IGW'}])
+        return igw
+    except ClientError as e:
+        print(e.response)
+        return None
+
+
+def attach_igw_to_vpc(client, igw_id, vpc_id):
+    try:
+        client.attach_internet_gateway(InternetGatewayId=igw_id, VpcId=vpc_id)
+        return True
+    except ClientError:
+        return False
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="CLI program that helps with VPC",
+        prog='main.py',
+        epilog='DEMO APP'
+    )
+
+    parser.add_argument('cmd', help='Command', choices=['vpc', 'igw', 'attach'])
+    parser.add_argument('--vpc_name', type=str)
+    parser.add_argument('--igw_name', type=str)
+    parser.add_argument('--vpc_id', type=str)
+    parser.add_argument('--igw_id', type=str)
+    args = parser.parse_args()
+
+    ec2 = boto3.resource('ec2')
+
+    if args.cmd == 'vpc':
+        vpc = create_vpc(ec2, args.vpc_name)
+        if vpc is not None:
+            print(f"Created VPC: {vpc.id}, {vpc.name}")
+    elif args.cmd == 'igw':
+        igw = create_igw(ec2, args.igw_name)
+        if igw is not None:
+            print(f"Created IGW: {igw.id}, {igw.name}")
+    elif args.cmd == 'attach':
+        if attach_igw_to_vpc(ec2, args.igw_id, args.vpc_id):
+            print("Successfully attached")
+
+
+if __name__ == '__main__':
+    main()
